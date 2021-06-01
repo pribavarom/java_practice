532529a41fbdcebef658e02dcdd766135c31f183
60081335 <roman.pribavkin@leroymerlin.ru>
2021-06-01 17:05:42 +0300

    Commit 2

--- src/test1.java
+++
@@ -1,9 +0,0 @@
-class MyProgram {
-    public static void main(String[] args) {
-        int x = 5, y = 3;
-        int z = ++x;
-        y += x--;
-        z+= --x + y++;
-        System.out.println(x + y + z);
-    }
-}
