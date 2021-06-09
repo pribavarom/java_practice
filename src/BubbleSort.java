c779e380b5721192f7489a6aa3877ff2815c3df3
60081335 <roman.pribavkin@leroymerlin.ru>
2021-06-09 17:13:32 +0300

    Commit 5

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,22 +1,38 @@
 import java.util.Arrays;
+import java.util.Scanner;
 public class BubbleSort {
+
     public static void main(String[] args) {
-        int [] mas = {11, 3, 14, 16, 7};
+
+        Scanner input = new Scanner(System.in); // Объявляем Scanner
+        System.out.println("Enter array length: ");
+        int size = input.nextInt(); // Читаем с клавиатуры размер массива и записываем в size
+        int array[] = new int[size]; // Создаём массив int размером в size
+        System.out.println("Insert array elements:");
+        /*Пройдёмся по всему массиву, заполняя его*/
+        for (int i = 0; i < size; i++) {
+            array[i] = input.nextInt(); // Заполняем массив элементами, введёнными с клавиатуры
+        }
+        System.out.print ("Inserted array elements:[");
+        for (int i = 0; i < size; i++) {
+            System.out.print (array[i] + " "); // Выводим на экран, полученный массив
+        }
+        System.out.println("]");
 
         boolean isSorted = false;
         int buf;
         while(!isSorted) {
             isSorted = true;
-            for (int i = 0; i < mas.length-1; i++) {
-                if(mas[i] > mas[i+1]){
+            for (int i = 0; i < array.length-1; i++) {
+                if(array[i] > array[i+1]){
                     isSorted = false;
 
-                    buf = mas[i];
-                    mas[i] = mas[i+1];
-                    mas[i+1] = buf;
+                    buf = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = buf;
                 }
             }
         }
-        System.out.println(Arrays.toString(mas));
+        System.out.println("Sorted array:" + Arrays.toString(array));
     }
 }
